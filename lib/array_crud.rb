2b6121b438571e496125bbe693adccb01381261a
Jamal Farah <moulayjam@gmail.com>
2019-06-18 21:37:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+  empty = []
 end
 
 def create_an_array
-  jamal = []
+  array_cool = ['Jamal', 'Farah', 'Hicham', 'Diva']
 end
 
 def add_element_to_end_of_array(array, element)
